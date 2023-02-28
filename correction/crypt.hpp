d422a866ba25a6f30957ab6d8e6bc317382d714d
Hugo Touzeau <hugo.touzeau@epitech.eu>
2023-02-28 15:40:02 +0100

    [ADD] crypt function

--- correction/crypt.hpp
+++ correction/crypt.hpp
@@ -2,5 +2,20 @@
 
 #include <string>
 
-std::string decrypt(std::string str, int key);
-
+std::string crypt(std::string str, int key) {
+    std::string resultat = "";
+    for (int i = 0; i < str.length(); i++) {
+        char caractere = str[i];
+        if (isalpha(caractere)) {
+            char minuscule = tolower(caractere);
+            char decale = ((minuscule - 'a') + key) % 26 + 'a';
+            if (isupper(caractere)) {
+                decale = toupper(decale);
+            }
+            resultat += decale;
+        } else {
+            resultat += caractere;
+        }
+    }
+    return resultat;
+}
