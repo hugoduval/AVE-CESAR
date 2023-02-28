ea12b7dfe3603da9682843a960dee1de0b65c218
Hugo Duval <hugoduval.gml@gmail.com>
2023-02-28 16:59:18 +0100

    [ADD] exercice à trou

--- exercice/crypt.hpp
+++ exercice/crypt.hpp
@@ -2,5 +2,29 @@
 
 #include <string>
 
-std::string decrypt(std::string str);
-
+/**
+ * @brief Chiffre une chaine de caractère avec une clé donnée
+ *
+ * @param str chaine de caractère à chiffrer
+ * @param key clé de chiffrement
+ * @return std::string résultat du chiffrement
+ */
+std::string crypt(std::string str, int key) {
+    std::string result; // Créer une variable de type string nommée result qui contiendra le résultat
+    for (int i = 0; /* */; i++) // Pour chaque caractère de la chaine de caractère
+    {
+        if (/* */) // Si le caractère est une minuscule
+        {
+            result += char(int(/* */) % /* */ + 'a'); // On ajoute le caractère décalé de key à result
+        }
+        else if (/* */) // Si le caractère est une majuscule
+        {
+            result += char(int(/* */) % /* */ + 'A'); // On ajoute le caractère décalé de key à result
+        }
+        else // Si le caractère n'est pas une lettre
+        {
+            result += str[i]; // On ajoute le caractère à result
+        }
+    }
+    return result; // On retourne result
+}
